2239bfe3fbefe66c7ff3925c3e7b242bf29327b1
pc <pc@JUUSO>
2023-11-19 23:01:00 +0200

    Add project files.

---
+++ CityMirrorFix/Patches/OnGameInit.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+namespace CityMirrorFix.Patches
+{
+    [HarmonyPatch(typeof(GorillaTagger), "Awake"), HarmonyWrapSafe]
+    internal class OnGameInit
+    {
+        public static void Postfix()
+        {
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/City").SetActive(true);
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/City/CosmeticsRoomAnchor/ShoppingCenterAnchor/mirrors2 (1)/CameraC").GetComponent<Camera>().cullingMask = GorillaTagger.Instance.thirdPersonCamera.transform.GetChild(0).gameObject.GetComponent<Camera>().cullingMask;
+            GameObject.Find("Environment Objects/LocalObjects_Prefab/City/CosmeticsRoomAnchor/ShoppingCenterAnchor/mirrors2 (1)/CameraC").GetComponent<Camera>().farClipPlane = 500f;
+        }
+    }
+}
---
+++ CityMirrorFix/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityMirrorFix
+{
+    [BepInPlugin("pl2w.citymirrorfix", "CityMirrorFix", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        Plugin() => new Harmony("pl2w.citymirrorfix").PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
